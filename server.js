455cb000fb59db86667857d9671ddfb48c7e2419
wenzhiTeo <wenzhi@infin8co.com>
2022-06-06 12:48:17 +0800

    public folder

--- server.js
+++ server.js
@@ -12,11 +12,10 @@ const PORT = process.env.PORT || 4000;
 server.start().then((res) => {
   server.applyMiddleware({ app, cors: true });
 
-  const publicPath = path.join(__dirname, "..", "public");
-  app.use(express.static(publicPath));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(publicPath, 'index.html'));
+  app.use(express.static('public'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
   });
-
+  
   app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 });
